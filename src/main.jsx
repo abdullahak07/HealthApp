0cd77715caaef07d1de6efb728e8f7e0154bba13
Abdullah Ahmad Khan <53994342+abdullahak07@users.noreply.github.com>
2026-06-28 01:15:09 +0800

    Show adaptive next-day gym guidance on Nutrition

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import SimpleHomeManagerV2 from "./SimpleHomeManagerV2";
+import AdaptiveNutritionCoach from "./AdaptiveNutritionCoach";
 import "./styles.css";
 import "./simple-home.css";
+import "./adaptive-nutrition-coach.css";
 import "./routineUploadEnhancer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SimpleHomeManagerV2>
       <App />
+      <AdaptiveNutritionCoach />
     </SimpleHomeManagerV2>
   </React.StrictMode>,
 );
