a41605a195efe541059bd8ce95378698ab993946
Abdullah Ahmad Khan <53994342+abdullahak07@users.noreply.github.com>
2026-06-28 01:22:03 +0800

    Use live food data and calorie offset guidance

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import SimpleHomeManagerV2 from "./SimpleHomeManagerV2";
-import AdaptiveNutritionCoach from "./AdaptiveNutritionCoach";
+import SimpleHomeManagerV3 from "./SimpleHomeManagerV3";
+import CalorieOffsetCoach from "./CalorieOffsetCoach";
 import "./styles.css";
 import "./simple-home.css";
-import "./adaptive-nutrition-coach.css";
+import "./calorie-offset.css";
 import "./routineUploadEnhancer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SimpleHomeManagerV2>
+    <SimpleHomeManagerV3>
       <App />
-      <AdaptiveNutritionCoach />
-    </SimpleHomeManagerV2>
+      <CalorieOffsetCoach />
+    </SimpleHomeManagerV3>
   </React.StrictMode>,
 );
