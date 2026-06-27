95372019c558910c495760de4895bc23497b04ce
Abdullah Ahmad Khan <53994342+abdullahak07@users.noreply.github.com>
2026-06-28 01:01:32 +0800

    Use the simplified Today dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import SimpleHomeManager from "./SimpleHomeManager";
 import "./styles.css";
+import "./simple-home.css";
 import "./routineUploadEnhancer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <SimpleHomeManager>
+      <App />
+    </SimpleHomeManager>
   </React.StrictMode>,
 );
