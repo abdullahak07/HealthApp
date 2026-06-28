5c4f6277722d9b50540e340e0ff15bd19dc949ff
Abdullah Ahmad Khan <53994342+abdullahak07@users.noreply.github.com>
2026-06-28 15:52:42 +0800

    Enable personal accounts and cloud persistence

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,23 @@ import "./profileDefaultsSync";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AccountCloudManager from "./AccountCloudManager";
 import SimpleHomeManagerV4 from "./SimpleHomeManagerV4";
 import CalorieOffsetCoach from "./CalorieOffsetCoach";
 import "./styles.css";
 import "./simple-home.css";
 import "./calorie-offset.css";
 import "./ai-food.css";
+import "./account-cloud.css";
 import "./routineUploadEnhancer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SimpleHomeManagerV4>
-      <App />
-      <CalorieOffsetCoach />
-    </SimpleHomeManagerV4>
+    <AccountCloudManager>
+      <SimpleHomeManagerV4>
+        <App />
+        <CalorieOffsetCoach />
+      </SimpleHomeManagerV4>
+    </AccountCloudManager>
   </React.StrictMode>,
 );
