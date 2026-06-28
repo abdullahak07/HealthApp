0a5e5d60c947664e05260c2f951b72ec8771539c
Abdullah Ahmad Khan <53994342+abdullahak07@users.noreply.github.com>
2026-06-28 15:36:40 +0800

    Support both Vercel and GitHub Pages builds

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/HealthApp/",
+  base: process.env.VERCEL ? "/" : "/HealthApp/",
   plugins: [react()],
 });
